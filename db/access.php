25e71f91aedc1a84c0e08094db637f9472c81747
nread <nick@equella.com>
2012-06-27 14:37:59 +1000

    Remove mod/equella:addinstance capability as it's for Moodle 2.3 only (doh!)

--- db/access.php
+++ db/access.php
@@ -26,16 +26,4 @@ $capabilities = array(
             'user' => CAP_ALLOW,
         )
     ),
-
-    'mod/equella:addinstance' => array(
-        'riskbitmask' => RISK_XSS,
-
-        'captype' => 'write',
-        'contextlevel' => CONTEXT_COURSE,
-        'archetypes' => array(
-            'editingteacher' => CAP_ALLOW,
-            'manager' => CAP_ALLOW
-        ),
-        'clonepermissionsfrom' => 'moodle/course:manageactivities'
-    ),
 );
