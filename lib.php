717148a26f4185a7fa32aa6f1c96a833b2e4876f
Dongsheng Cai <imdongsheng@gmail.com>
2012-08-28 16:16:44 +1000

    Refs #7070: Removed dnd hook, fixed function call

--- lib.php
+++ lib.php
@@ -264,6 +264,7 @@ function equella_replace_contents_with_references($file, $info) {
 
 function equella_module_event_handler($event) {
     global $CFG;
+    flog($CFG->equella_intercept_moodle_files);
     if (empty($CFG->equella_intercept_moodle_files)) {
         return;
     }
@@ -282,25 +283,28 @@ function equella_module_event_handler($event) {
  * Handle module created event
  */
 function equella_handle_mod_created($event) {
-    return equella_module_event_handler();
+    return equella_module_event_handler($event);
 }
 
 /**
  * Handle module updated event
  */
 function equella_handle_mod_updated($event) {
-    return equella_module_event_handler();
+    return equella_module_event_handler($event);
 }
 
 /**
  * Register the ability to handle drag and drop file uploads
  * @return array containing details of the files / types the mod can handle
  */
+
+/**
 function equella_dndupload_register() {
     return array('files' => array(
                      array('extension' => '*', 'message' => get_string('dnduploadresource', 'mod_equella'))
                  ));
 }
+*/
 
 /**
  * Handle a file that has been uploaded
