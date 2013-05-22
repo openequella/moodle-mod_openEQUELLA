75e4a259f22a539b7176c44dba53b3a05d3d09d9
Dongsheng Cai <dongsheng.cai@pearson.com>
2013-05-22 13:35:15 +1000

    Refs #8169 Fixed option header language string

--- lang/en/equella.php
+++ lang/en/equella.php
@@ -43,6 +43,7 @@ $string['restapinolocation'] = 'No location returned';
 ////////////////////////////////////////////////////////
 // OPTIONS
 
+$string['option.pagewindow.header'] = 'Display options';
 $string['option.pagewindow'] = 'Display';
 $string['option.pagewindow.same'] = 'Same window';
 $string['option.pagewindow.new'] = 'New window';
