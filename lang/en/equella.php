78dea249a01f88ed881d24a670a66e3390816299
Dongsheng Cai <dongsheng.cai@pearson.com>
2013-06-20 18:27:54 +1000

    Refs #8178 Some help text explains restrictions

--- lang/en/equella.php
+++ lang/en/equella.php
@@ -58,7 +58,7 @@ $string['config.action.desc'] = 'The action string for EQUELLA. Please note:
    2. There should not be a ? or a & at the start or end of the string.';
 
 $string['config.restriction.title'] = 'Restrict selections';
-$string['config.restriction.desc'] = 'Choose whether course editors should only be able to select items, attachments, packages or anything.';
+$string['config.restriction.desc'] = 'Choose whether course editors should only be able to select items, attachments, packages or anything. Please note that the restrictions only working for EQUELLA 6.0 and higher.';
 $string['config.restriction.none'] = 'No restrictions';
 
 $string['config.restriction.itemsonly'] = 'Items only';
