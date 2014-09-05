6ffadb711c3a35e78a7550ea622189b33cd5e197
Dongsheng Cai <dongsheng.cai@pearson.com>
2014-09-05 15:33:09 +1000

    Cleanup linkcheck.php code

--- linkcheck.php
+++ linkcheck.php
@@ -14,7 +14,7 @@
 //
 // You should have received a copy of the GNU General Public License
 // along with Moodle. If not, see <http://www.gnu.org/licenses/>.
-define('CLI_SCRIPT', true);
+
 require_once (dirname(dirname(dirname(__FILE__))) . '/config.php');
 require_once ($CFG->dirroot . '/mod/equella/common/lib.php');
 require_once ($CFG->dirroot . '/course/lib.php');
@@ -43,9 +43,8 @@ $disable_links = false;
 $notify = array('some.user');
 
 // ///////////////////////////////////////////////////////////
-// ///////////////////////////////////////////////////////////
 
-if ($password == '') {
+if (empty($password)) {
     echo 'EQUELLA link checking has not been configured.  Please see the source code for this page.';
     exit();
 }
@@ -56,36 +55,37 @@ if ($password_param != $password) {
     exit();
 }
 
-global $DB, $CFG;
-
-// Re-use the same CURL instance to maintain the same EQUELLA session for all URL checks
-$ch = curl_init();
-curl_setopt($ch, CURLOPT_NOBODY, true);
-curl_setopt($ch, CURLOPT_FAILONERROR, true);
-curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
-$temp = tmpfile();
-curl_setopt($ch, CURLOPT_COOKIEFILE, $temp);
-curl_setopt($ch, CURLOPT_COOKIEJAR, $temp);
+$http = new curl(array('cookie'=>true));
 
 echo '<style>.ok {color: green;} .bad {color: red;}</style><ul>';
 
 foreach($DB->get_records('equella') as $resource) {
 
-    curl_setopt($ch, CURLOPT_URL, equella_appendtoken($resource->url, equella_getssotoken_api()));
+    $url = equella_appendtoken($resource->url, equella_getssotoken_api());
+    $http->head($url);
+
+    $info = $http->get_info();
+    $statuscode = $info['http_code'];
 
     echo '<li>Checking <a href="' . $resource->url . '">' . $resource->url . '</a><br>';
-    if (curl_exec($ch)) {
+    if ((int)$statuscode == 200) {
         echo '<span class="ok">OK</span>';
     } else {
         echo '<span class="bad">Could not find in EQUELLA</span><br>';
-
         // tell someone - get users with course edit perms for the course in question
         $recipients = $DB->get_records_list('user', 'username', $notify);
         if ($recipients) {
             $from = get_admin();
             $subject = get_string('checker.subject', 'equella');
             $course = $DB->get_record('course', array('id' => $resource->course));
-            $message = get_string('checker.message', 'equella', array('name' => $resource->name,'url' => $resource->url,'coursename' => $course->shortname,'courseurl' => $CFG->wwwroot . '/course/view.php?id=' . $course->id));
+            $courseurl = new moodle_url('/course/view.php', array('id'=>$course->id));
+            $message = get_string('checker.message', 'equella', array(
+                            'name' => $resource->name,
+                            'url' => $resource->url,
+                            'coursename' => $course->shortname,
+                            'courseurl' => $courseurl
+                        )
+                );
 
             echo 'Emailing the following users:<ul>';
             foreach($recipients as $recipient) {
@@ -98,13 +98,12 @@ foreach($DB->get_records('equella') as $resource) {
         }
 
         // now mark the resource as hidden
+        $sql = "SELECT cm.*
+                  FROM {course_modules} cm
+                       INNER JOIN {modules} m ON cm.module = m.id
+                 WHERE m.name = :modname AND cm.course = :course AND cm.instance = :resource";
         if ($disable_links) {
-            $cms = $DB->get_records_sql("SELECT cm.* FROM {course_modules} cm
-                INNER JOIN {modules} m ON cm.module = m.id
-                WHERE m.name = :modname
-                AND cm.course = :course
-                AND cm.instance = :resource", array('modname' => 'equella','course' => $resource->course,'resource' => $resource->id));
-
+            $cms = $DB->get_records_sql($sql, array('modname' => 'equella','course' => $resource->course,'resource' => $resource->id));
             foreach($cms as $cm) {
                 set_coursemodule_visible($cm->id, 0);
             }
@@ -114,80 +113,4 @@ foreach($DB->get_records('equella') as $resource) {
     echo '</li>';
 }
 
-curl_close($ch);
-
 echo '</ul>';
-
-// The following method is currently disabled/deprecated due to incorrect users being emailed in some cases.
-// Future investigations will be made to determine what the problem is here and correct it.
-function get_course_editors($courseid) {
-    global $DB;
-
-    // get the 'course editors' with the lowest level context
-    $capability = 'moodle/course:manageactivities';
-
-    // course level editors
-    $course_editors = $DB->get_records_sql("SELECT u.* FROM {user} u
-        INNER JOIN {role_assignments} ra ON u.id = ra.userid
-        INNER JOIN {context} x ON ra.contextid = x.id
-        INNER JOIN {course} c ON x.instanceid = c.id
-        INNER JOIN {role} r ON ra.roleid = r.id
-        INNER JOIN {role_capabilities} rc ON r.id = rc.roleid AND ra.contextid = rc.contextid
-        WHERE rc.capability = :capability
-        AND x.contextlevel = :contextlevel
-        AND c.id = :courseid
-        AND u.deleted = 0
-        AND u.emailstop = 0", array('contextlevel' => CONTEXT_COURSE,'capability' => $capability,'courseid' => $courseid));
-    if ($course_editors) {
-        return $course_editors;
-    }
-
-    // cat level editors
-    $course_editors = $DB->get_records_sql("SELECT u.* FROM {user} u
-        INNER JOIN {role_assignments} ra ON u.id = ra.userid
-        INNER JOIN {context} x ON ra.contextid = x.id
-        INNER JOIN {course_categories} cc ON x.instanceid = cc.id
-        INNER JOIN {course} c ON cc.id = c.category
-        INNER JOIN {role} r ON ra.roleid = r.id
-        INNER JOIN {role_capabilities} rc ON r.id = rc.roleid AND ra.contextid = rc.contextid
-        WHERE rc.capability = :capability
-        AND x.contextlevel = :contextlevel
-        AND c.id = :courseid
-        AND u.deleted = 0
-        AND u.emailstop = 0", array('contextlevel' => CONTEXT_COURSECAT,'capability' => $capability,'courseid' => $courseid));
-    if ($course_editors) {
-        return $course_editors;
-    }
-
-    // global level editors not admin
-    $course_editors = $DB->get_records_sql("SELECT u.* FROM {user} u
-        INNER JOIN {role_assignments} ra ON u.id = ra.userid
-        INNER JOIN {context} x ON ra.contextid = x.id
-        INNER JOIN {role} r ON ra.roleid = r.id
-        INNER JOIN {role_capabilities} rc ON r.id = rc.roleid AND ra.contextid = rc.contextid
-        WHERE rc.capability = :capability
-        AND x.contextlevel = :contextlevel
-        AND r.name != 'Administrator'
-        AND u.deleted = 0
-        AND u.emailstop = 0", array('contextlevel' => CONTEXT_SYSTEM,'capability' => $capability));
-    if ($course_editors) {
-        return $course_editors;
-    }
-
-    // global admins
-    $course_editors = $DB->get_records_sql("SELECT u.* FROM {user} u
-        INNER JOIN {role_assignments} ra ON u.id = ra.userid
-        INNER JOIN {context} x ON ra.contextid = x.id
-        INNER JOIN {role} r ON ra.roleid = r.id
-        INNER JOIN {role_capabilities} rc ON r.id = rc.roleid AND ra.contextid = rc.contextid
-        WHERE rc.capability = :capability
-        AND x.contextlevel = :contextlevel
-        AND r.name = 'Administrator'
-        AND u.deleted = 0
-        AND u.emailstop = 0", array('contextlevel' => CONTEXT_SYSTEM,'capability' => $capability));
-    if ($course_editors) {
-        return $course_editors;
-    }
-
-    return false;
-}
