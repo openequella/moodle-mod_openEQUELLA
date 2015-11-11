34f49409924043ba808f1ef3f53e9f9521bd6de6
Dongsheng Cai <dongsheng.cai@pearson.com>
2015-11-11 14:34:19 +1100

    EQ-2439 github #37 moodle 3.0 version.php

--- version.php
+++ version.php
@@ -15,6 +15,6 @@
 // along with Moodle. If not, see <http://www.gnu.org/licenses/>.
 defined('MOODLE_INTERNAL') || die();
 
-$module->version   = 2015042000;
-$module->requires  = 2014041101;    // Requires this Moodle version
-$module->component = 'mod_equella'; // Full name of the plugin (used for diagnostics)
+$plugin->version   = 2015042000;
+$plugin->requires  = 2014041101;    // Requires this Moodle version
+$plugin->component = 'mod_equella'; // Full name of the plugin (used for diagnostics)
