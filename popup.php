f01f36e162adcb8d4b234890fceb9f14ee5dcad2
siddharth-edalex <siddharth@edalex.com>
2025-02-26 11:05:22 +1100

    Import the lib.php file

--- popup.php
+++ popup.php
@@ -14,7 +14,7 @@
 // You should have received a copy of the GNU General Public License
 // along with Moodle. If not, see <http://www.gnu.org/licenses/>.
 require_once ('../../config.php');
-
+require_once($CFG->dirroot . '/mod/equella/lib.php');
 require_login();
 
 $cmid = required_param('cmid', PARAM_INT);
